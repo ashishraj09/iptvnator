9dde861c3096f920f9fb78148ae021a2dd04d0bd
Ashish Raj <ashishraj09@gmail.com>
2024-11-11 12:06:38 +1300

    feat: set external db to true

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
-    production: false,
+    production: true,
     environment: 'WEB',
     version: require('../../package.json').version,
     BACKEND_URL: 'http://localhost:3333',
-    ENABLE_EXTERNAL_DB: false,
+    ENABLE_EXTERNAL_DB: true,
 };
